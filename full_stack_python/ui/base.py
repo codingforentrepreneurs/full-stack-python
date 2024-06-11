5ab609364e18a750277285402dcca6b45700da70
CodingForEntrepreneurs <hello@teamcfe.com>
2024-06-11 10:48:28 -0600

    8 - Better Code Management

---
+++ full_stack_python/ui/base.py
@@ -0,0 +1,20 @@
+import reflex as rx
+
+from .nav import navbar
+
+def base_page(child: rx.Component, hide_navbar=False, *args, **kwargs) -> rx.Component:
+    # print([type(x) for x in args])
+    if not isinstance(child,rx. Component):
+        child = rx.heading("this is not a valid child element")
+    if hide_navbar:
+        return rx.container(
+            child,
+            rx.logo(),
+            rx.color_mode.button(position="bottom-left"),
+    )
+    return rx.container(
+        navbar(),
+        child,
+        rx.logo(),
+        rx.color_mode.button(position="bottom-left"),
+    )
---
+++ full_stack_python/ui/nav.py
@@ -0,0 +1,86 @@
+import reflex as rx
+
+def navbar_link(text: str, url: str) -> rx.Component:
+    return rx.link(
+        rx.text(text, size="4", weight="medium"), href=url
+    )
+
+
+def navbar() -> rx.Component:
+    return rx.box(
+        rx.desktop_only(
+            rx.hstack(
+                rx.hstack(
+                    rx.image(
+                        src="/logo.jpg",
+                        width="2.25em",
+                        height="auto",
+                        border_radius="25%",
+                    ),
+                    rx.heading(
+                        "Reflex", size="7", weight="bold"
+                    ),
+                    align_items="center",
+                ),
+                rx.hstack(
+                    navbar_link("Home", "/#"),
+                    navbar_link("About", "/#"),
+                    navbar_link("Pricing", "/#"),
+                    navbar_link("Contact", "/#"),
+                    spacing="5",
+                ),
+                rx.hstack(
+                    rx.button(
+                        "Sign Up",
+                        size="3",
+                        variant="outline",
+                    ),
+                    rx.button("Log In", size="3"),
+                    spacing="4",
+                    justify="end",
+                ),
+                justify="between",
+                align_items="center",
+            ),
+        ),
+        rx.mobile_and_tablet(
+            rx.hstack(
+                rx.hstack(
+                    rx.image(
+                        src="/logo.jpg",
+                        width="2em",
+                        height="auto",
+                        border_radius="25%",
+                    ),
+                    rx.heading(
+                        "Reflex", size="6", weight="bold"
+                    ),
+                    align_items="center",
+                ),
+                rx.menu.root(
+                    rx.menu.trigger(
+                        rx.icon("menu", size=30)
+                    ),
+                    rx.menu.content(
+                        rx.menu.item("Home"),
+                        rx.menu.item("About"),
+                        rx.menu.item("Pricing"),
+                        rx.menu.item("Contact"),
+                        rx.menu.separator(),
+                        rx.menu.item("Log in"),
+                        rx.menu.item("Sign up"),
+                    ),
+                    justify="end",
+                ),
+                justify="between",
+                align_items="center",
+            ),
+        ),
+        bg=rx.color("accent", 3),
+        padding="1em",
+        # position="fixed",
+        # top="0px",
+        # z_index="5",
+        width="100%",
+    )
+
