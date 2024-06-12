6fa9aed7a898e1152bb9f914ac3f9373fe27b564
CodingForEntrepreneurs <hello@teamcfe.com>
2024-06-12 12:16:18 -0600

    16 - Contact Form

--- full_stack_python/pages/contact.py
+++ full_stack_python/pages/contact.py
@@ -3,13 +3,48 @@ import reflex as rx
 from .. import navigation
 from ..ui.base import base_page
 
+
+class ContactState(rx.State):
+    from_data: dict = {}
+
+    def handle_submit(self, form_data: dict):
+        """Handle the form submit."""
+        print(form_data)
+        self.form_data = form_data
+
 @rx.page(route=navigation.routes.CONTACT_US_ROUTE)
 def contact_page() -> rx.Component:
+    my_form = rx.form(
+            rx.vstack(
+                rx.input(
+                    name="first_name",
+                    placeholder="First Name",
+                    required=True,
+                    type='text'
+                ),
+                rx.input(
+                    name="last_name",
+                    placeholder="Last Name",
+                    type='text'
+                ),
+                rx.input(
+                    name='email',
+                    placeholder='Your email',
+                    type='email',
+                ),
+                rx.text_area(
+                    name='message',
+                    placeholder="Your message",
+                    required=True
+                ),
+                rx.button("Submit", type="submit"),
+            ),
+            on_submit=ContactState.handle_submit,
+            reset_on_submit=True,
+    )
     my_child = rx.vstack(
             rx.heading("Contact Us", size="9"),
-            rx.text(
-                "Something cool about us.",
-            ),
+            my_form,
             spacing="5",
             justify="center",
             align="center",
