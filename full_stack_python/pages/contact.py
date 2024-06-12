768c30eb21f5a08e2742473ee67af7c72323d39d
CodingForEntrepreneurs <hello@teamcfe.com>
2024-06-12 17:05:02 -0600

    24 - Adding a DateTime Field

--- full_stack_python/pages/contact.py
+++ full_stack_python/pages/contact.py
@@ -1,16 +1,30 @@
+from datetime import datetime, timezone
+
 import asyncio
 import reflex as rx 
 
-# from sqlmodel import Field
+import sqlalchemy
+from sqlmodel import Field
 
 from .. import navigation
 from ..ui.base import base_page
+
+def get_utc_now() -> datetime:
+    return datetime.now(timezone.utc)
 
 class ContactEntryModel(rx.Model, table=True):
     first_name: str
     last_name: str | None = None
     email: str | None = None # = Field(nullable=True)
     message: str
+    created_at: datetime = Field(
+        default_factory=get_utc_now,
+        sa_type=sqlalchemy.DateTime(timezone=True),
+        sa_column_kwargs={
+            'server_default': sqlalchemy.func.now()
+        },
+        nullable=False
+    )
 
 
 class ContactState(rx.State):
