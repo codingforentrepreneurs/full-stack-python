d4b8ce75b8565ad12251c6213c7a99d8912147a0
CodingForEntrepreneurs <hello@teamcfe.com>
2024-06-13 11:03:35 -0600

    28 - State for Listing Blog Post Data

---
+++ full_stack_python/blog/state.py
@@ -0,0 +1,25 @@
+from typing import Optional, List
+import reflex as rx 
+
+from sqlmodel import select
+
+from .model import BlogPostModel
+
+class BlogPostState(rx.State):
+    posts: List['BlogPostModel'] = []
+    post: Optional['BlogPostModel'] = None
+
+    def load_posts(self):
+        with rx.session() as session:
+            result = session.exec(
+                select(BlogPostModel)
+            )
+            self.posts = result
+        # return
+
+    # def get_post(self):
+    #     with rx.session() as session:
+    #         result = session.exec(
+    #             select(BlogPostModel)
+    #         )
+    #         self.posts = result
